1cded940adb2a3a5d6060c5303511dab8932fcfa
andre-zh <andre.zh@foxmail.com>
2019-02-25 20:56:54 +1100

    Init project

---
+++ src/test/java/com/RecipeFinder/RecipeFinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.RecipeFinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeFinderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
